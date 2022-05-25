a6782be4351613f6f92bd791d064529f780e8d6e
ganesh-narole-au46 <100786117+ganesh-narole-au46@users.noreply.github.com>
2022-05-25 08:20:09 +0530

    Day 1
    1st day of learning JavaScript

---
+++ GN FSD.js
@@ -0,0 +1,20 @@
+let a=5;      //declaring let
+let b=6;
+c= a+b      //adding two numbers
+console.log(c)  //result
+
+var x=2;
+var y= 5;
+z= x*y    //multiplying two variables
+console.log(z)
+
+d= b-a   //subtracting two numbers
+console.log(d)
+
+e= y/x  // dividing two numbers
+console.log(e)
+
+const i= 2
+let j=3
+k= i==j  // comparing two numbers
+console.log(k)
